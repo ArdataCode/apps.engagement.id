b0c6736e4967be5c68909573b691c3335d84d444
ArdataCode <ardata.indonesia@gmail.com>
2024-05-16 04:52:11 +0000

    first

---
+++ public_html/app/config.php
@@ -0,0 +1,8 @@
+<?php
+//Config Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'u726706882_engage');
+define('DB_PASS', 'Ardata2023');
+define('DB_NAME', 'u726706882_engage');
+define('TIMEZONE', 'Asia/Jakarta');
+define('ENCRYPTION_KEY', '8d17c3d3dc5527bf5a353fe3e41e3bbf');
